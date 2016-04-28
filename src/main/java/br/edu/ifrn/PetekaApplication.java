bb0a3943c350e354b567ed83f4ab36d63e725895
Duarte Fernandes <duarte_fq@hotmail.com>
2016-04-28 15:27:12 -0300

    Bootstrap project using Spring Initializr

---
+++ src/main/java/br/edu/ifrn/PetekaApplication.java
@@ -0,0 +1,12 @@
+package br.edu.ifrn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetekaApplication.class, args);
+	}
+}
