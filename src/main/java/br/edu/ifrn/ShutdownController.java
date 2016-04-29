22d57e8538f4cf5b9cb74830156f47ebe9e6c074
Duarte Fernandes <duarte_fq@hotmail.com>
2016-04-29 00:54:28 -0300

    [GET] /sair

---
+++ src/main/java/br/edu/ifrn/ShutdownController.java
@@ -0,0 +1,19 @@
+package br.edu.ifrn;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@RestController
+class ShutdownController {
+
+    @Autowired
+    private ApplicationContext appContext;
+
+    @RequestMapping("/sair")
+    public void initiateShutdown(){
+        ((ConfigurableApplicationContext)appContext).close();
+    }
+}
