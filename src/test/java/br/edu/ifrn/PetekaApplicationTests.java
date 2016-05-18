31a7d92475325bb08fdd4f580982a44c8bbdc9aa
JoabMendes <joab.mendes.r2d2@gmail.com>
2016-05-18 17:35:30 -0300

    Fix conflicts

--- src/test/java/br/edu/ifrn/PetekaApplicationTests.java
+++ src/test/java/br/edu/ifrn/PetekaApplicationTests.java
@@ -1,5 +1,6 @@
 package br.edu.ifrn;
 
+import br.edu.ifrn.peteka.PetekaApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
