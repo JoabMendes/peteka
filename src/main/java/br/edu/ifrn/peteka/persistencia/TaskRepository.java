e729b185a791d0d49db2e2967ca8a4f96f87c445
Duarte Fernandes <duarte_fq@hotmail.com>
2016-06-01 21:25:19 -0300

    Closes #52

---
+++ src/main/java/br/edu/ifrn/peteka/persistencia/TaskRepository.java
@@ -0,0 +1,9 @@
+package br.edu.ifrn.peteka.persistencia;
+
+import br.edu.ifrn.peteka.dominio.Task;
+
+/**
+ * Created by duartemac on 2016-06-01.
+ */
+public class TaskRepository extends RepositoryMemory<Long, Task> {
+}
---
+++ src/main/java/br/edu/ifrn/peteka/persistencia/UserRepository.java
@@ -0,0 +1,9 @@
+package br.edu.ifrn.peteka.persistencia;
+
+import br.edu.ifrn.peteka.dominio.User;
+
+/**
+ * Created by duartemac on 2016-06-01.
+ */
+public class UserRepository extends RepositoryMemory<Long, User> {
+}
