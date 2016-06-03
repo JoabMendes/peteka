664720bf6387abe7262a1d63b1e40f9eebb4c9cd
JoabMendes <joab.mendes.r2d2@gmail.com>
2016-06-03 00:10:57 -0300

    Criou teste de integração para cada uma das classes de serviço das classes de domínio .Closes #57

---
+++ src/test/java/br/edu/ifrn/peteka/servico/StatusServiceTest.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifrn.peteka.servico;
+
+import br.edu.ifrn.peteka.PetekaApplication;
+import br.edu.ifrn.peteka.dominio.Status;
+import javax.inject.Inject;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.testng.annotations.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ *
+ * @author joab
+ */
+@SpringApplicationConfiguration(classes = PetekaApplication.class)
+@WebAppConfiguration
+@Test
+public class StatusServiceTest extends AbstractTestNGSpringContextTests {
+    
+    @Inject
+    private StatusService statusService;
+    private final String STATUS_LABEL = "label";
+    
+    public void testServiceIsNotNull(){
+        assertThat(statusService).isNotNull();
+    }
+    
+    public void testSaveOne(){
+        // Creates the test environment
+        Status status = Status.builder().label(this.STATUS_LABEL).build();
+        
+        // Saves
+        this.statusService.save(status);
+        
+        // Verifies if saved
+        assertThat(this.statusService.iterator().next()).isEqualTo(status);
+        
+    }
+    
+    
+    public void testDeleteOne(){
+        // Creates the test environment
+        Status status = Status.builder().label(this.STATUS_LABEL).build();
+        
+        // Saves
+        this.statusService.save(status);
+        
+        // Deletes
+        this.statusService.delete(status);
+        
+        //Test if deleted
+        assertThat(this.statusService.iterator().hasNext()).isFalse();
+    }
+    
+}
---
+++ src/test/java/br/edu/ifrn/peteka/servico/TaskServiceTest.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifrn.peteka.servico;
+
+import br.edu.ifrn.peteka.PetekaApplication;
+import br.edu.ifrn.peteka.dominio.Task;
+import javax.inject.Inject;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.testng.annotations.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ *
+ * @author joab
+ */
+@SpringApplicationConfiguration(classes = PetekaApplication.class)
+@WebAppConfiguration
+@Test
+public class TaskServiceTest extends AbstractTestNGSpringContextTests  {
+    
+    @Inject
+    private TaskService taskService;
+
+    private final String TASK_TITLE = "title";
+    private final String TASK_DESCRIPTION = "D1";
+    
+    public void testServiceIsNotNull(){
+        assertThat(taskService).isNotNull();
+    }
+    
+    public void testSaveOne(){
+        // Creates the test environment
+        Task task = Task.builder()
+                .title(this.TASK_TITLE)
+                .description(this.TASK_DESCRIPTION).build();
+        
+        // Saves
+        this.taskService.save(task);
+        
+        // Verifies if saved
+        assertThat(this.taskService.iterator().next()).isEqualTo(task);
+        
+    }
+    
+    
+    public void testDeleteOne(){
+        // Creates the test environment
+        Task task = Task.builder()
+                .title(this.TASK_TITLE)
+                .description(this.TASK_DESCRIPTION).build();
+        this.taskService.save(task);
+        
+        // Deletes
+        this.taskService.delete(task);
+        
+        //Test if deleted
+        assertThat(this.taskService.iterator().hasNext()).isFalse();
+    }
+    
+    
+}
